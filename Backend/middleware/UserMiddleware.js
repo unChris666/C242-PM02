f36efdf993785fb54f74ba7c1c8db19b1660d2ea
Axel <axeldarren.suryanto@gmail.com>
2024-12-12 22:09:26 +0700

    backend middleware auth if header and else cookie

--- Backend/middleware/UserMiddleware.js
+++ Backend/middleware/UserMiddleware.js
@@ -4,10 +4,12 @@ const { User, Role } = require('../models');
 
 exports.authMiddleware = async (req,res,next) => {
     // 1. fungsi jika di header kita masukan token atau tidak
-    let token = req.cookies.jwt;
-    // if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-    //     token = req.headers.authorization.split(" ")[1];
-    // }
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        token = req.headers.authorization.split(" ")[1];
+    } else if(req.cookies.jwt){
+        let token = req.cookies.jwt;
+    }
+    
 
     if (!token) {
         return res.status(401).json({
